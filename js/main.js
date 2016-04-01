dec5415736f36c49e145753b611a2c5a84bcb7bb
[Abigail Pendergrass] <[pendergrass.abigail@gmail.com]>
2016-04-01 08:07:49 -0400

    create contact-bar js file to put drop function in, put script link in bottom of index.html

--- js/main.js
+++ js/main.js
@@ -16,23 +16,3 @@ function handler(e) {
 
   e.stopPropagation();
 }
-
-var myContactBar = document.getElementById('contact-bar');
-var myContactNavItem = document.querySelector('[href="#contact"]');
-
-myContactNavItem.addEventListener("click", handler, false);
-
-function handler() {
-
-  if (myContactBar.classList.contains('close')) {
-
-  myContactBar.classList.remove('close');
-  myContactBar.classList.add('open');
-  }
-
-  else {
-
-    myContactBar.classList.remove('open');
-    myContactBar.classList.add('close');
-  }
-}
